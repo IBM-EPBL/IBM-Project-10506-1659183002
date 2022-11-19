072e765eab91013abcfe1be85c88a36d89f692fc
BLooDBRothER <computertechnology446@gmail.com>
2022-11-19 08:11:43 +0530

    Add sprint 3

---
+++ Project Development Phase/sprint 3/frontend/js/validate_token.js
@@ -0,0 +1,33 @@
+import { endpoint } from "./modules/endpoint.js";
+
+const msgCnt = document.querySelector(".confirm-msg");
+
+const successTemplate = "<p>E-Mail verfied</p><a href='./index.html'>Home</a>"
+const verify_email = async (token) => {
+    const res = await fetch(endpoint.confirm_token, {
+        method: "POST",
+        credentials: "include",
+        mode:"cors",
+        headers:{
+            "Access-Control-Allow-Origin":"*",
+            "content-type":"application/json",
+        },
+        body: JSON.stringify({
+            "token": token
+        })
+    })
+    // const data = res.json();
+    // console.log(res.status)
+    if(res.status == 200){
+        msgCnt.innerHTML = successTemplate;
+        return;
+    }
+    msgCnt.innerHTML = "Verification Failed Resend Email";
+}
+
+window.addEventListener("load", () => {
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    const token = urlParams.get('token');
+    verify_email(token);
+})
